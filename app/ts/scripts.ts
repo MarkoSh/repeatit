58cc7644872655fe96a88b85faf3c2d14a77bf66
MarkoSh <markhost@yandex.ru>
2019-01-13 20:26:46 +0300

    New Age

---
+++ app/ts/scripts.ts
@@ -0,0 +1,5 @@
+( ( dom, ls ) => {
+
+    
+
+} )( document, localStorage );
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp          = require( "gulp" ),
+var fs			  = require( "fs" ),
+	gulp          = require( "gulp" ),
 	gutil         = require( "gulp-util" ),
 	scss          = require( "gulp-sass" ),
 	browsersync   = require( "browser-sync" ),
@@ -8,62 +9,80 @@ var gulp          = require( "gulp" ),
 	rename        = require( "gulp-rename" ),
 	autoprefixer  = require( "gulp-autoprefixer" ),
 	notify        = require( "gulp-notify" ),
+	typescript 	  = require( "gulp-tsc" ),
 	rsync         = require( "gulp-rsync" ),
 	gcmq          = require( "gulp-group-css-media-queries" ),
 	htmlmin		  = require( "gulp-htmlmin" );
 
-gulp.task( "browser-sync", function() {
+let styles = () => {
+	return gulp.src( "app/scss/main.scss" )
+		.pipe( scss( { outputStyle: "expand" } ).on( "error", notify.onError() ) )
+		.pipe( rename( { suffix: ".min", prefix : "" }))
+		.pipe( autoprefixer( [ "last 15 versions" ] ).on( "error", notify.onError() ) )
+		.pipe( cleancss( { level: { 1: { specialComments: 0 } } } ).on( "error", notify.onError() ) ) // Opt., comment out when debugging
+		.pipe( gulp.dest( "app/css" ) )
+		.pipe( browsersync.reload( { stream: true } ) );
+}
+
+let ts = () => {
+	return gulp.src( "app/ts/scripts.ts" )
+		.pipe( typescript().on( "error", notify.onError() ) )
+		.pipe( gulp.dest( "app/js" ) );
+}
+
+let js = () => {
+	return gulp.src( [
+			"node_modules/axios/dist/axios.min.js",
+
+			"node_modules/inputmask/dist/min/inputmask/dependencyLibs/inputmask.dependencyLib.min.js",
+			"node_modules/inputmask/dist/min/inputmask/inputmask.min.js",
+			"node_modules/inputmask/dist/min/inputmask/inputmask.extensions.min.js",
+			"node_modules/inputmask.phone/dist/min/inputmask.phone/inputmask.phone.extensions.min.js",
+
+			"node_modules/photoswipe/dist/photoswipe.min.js",
+			"node_modules/photoswipe/dist/photoswipe-ui-default.min.js",
+
+			"node_modules/swiper/dist/js/swiper.min.js",
+
+			"app/js/scripts.js", // Always at the end
+		] )
+		.pipe( concat( "scripts.min.js" ) )
+		.pipe( uglify().on( "error", notify.onError() ) ) // Mifify js (opt.) - mifify hahaha
+		.pipe( gulp.dest( "app/js" ) )
+		.pipe( browsersync.reload( { stream: true } ) );
+}
+
+let browser_sync = () => {
 	browsersync( {
 		server: {
-			baseDir: "app"
+			baseDir: 'app'
 		},
-		notify: false,
-		// open: false,
-		// tunnel: true,
-		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
-	} )
-} );
+		notify: false
+	} );
+}
 
-gulp.task( "scss", function() {
-	return gulp.src( "app/scss/**/*.scss" )
-	.pipe( scss( { outputStyle: "expand" } ).on( "error", notify.onError() ) )
-	.pipe( rename( { suffix: ".min", prefix : "" }))
-	.pipe( autoprefixer( [ "last 15 versions" ] ) )
-	.pipe( cleancss( { level: { 1: { specialComments: 0 } } } ) ) // Opt., comment out when debugging
-	.pipe( gulp.dest( "app/css" ) )
-	.pipe( browsersync.reload( { stream: true } ) )
-} );
+let watchFiles = () => {
+	gulp.watch( "**/*.scss", styles );
+	gulp.watch( "app/ts/scripts.ts", ts );
+	gulp.watch( "app/js/scripts.js", js );
+	gulp.watch( "app/index.html", browsersync.reload );
 
-gulp.task( "js", function() {
-	return gulp.src( [
-		"app/libs/jquery/dist/jquery.min.js",
-		"app/js/common.js", // Always at the end
-		] )
-	.pipe( concat( "scripts.min.js" ) )
-	.pipe( uglify() ) // Mifify js (opt.) - mifify hahaha
-	.pipe( gulp.dest( "app/js" ) )
-	.pipe( browsersync.reload( { stream: true } ) )
-} );
 
-gulp.task( "rsync", function() {
-	return gulp.src( "app/**" )
-	.pipe( rsync({
-		root		: "app/",
-		hostname	: "username@yousite.com",
-		destination	: "yousite/public_html/",
-		// include	: [ "*.htaccess" ], // Includes files to deploy
-		exclude		: [ "**/Thumbs.db", "**/*.DS_Store" ], // Excludes files from deploy
-		recursive	: true,
-		archive		: true,
-		silent		: false,
-		compress	: true
-	} ) )
-} );
+	fs.watchFile( "app/css/style.min.css", {
+		interval: 100
+	}, ( current, previous ) => {
+		if ( current.size == 0 ) {
+			gulp.parallel( styles );
+		}
+	} );
 
-gulp.task( "watch", [ "scss", "js", "browser-sync" ], function() {
-	gulp.watch( "app/scss/**/*.scss", [ "scss"] );
-	gulp.watch( [ "libs/**/*.js", "app/js/common.js" ], [ "js" ] );
-	gulp.watch( "app/*.html", browsersync.reload )
-});
+	fs.watchFile( "app/js/scripts.min.js", {
+		interval: 100
+	}, ( current, previous ) => {
+		if ( current.size == 0 ) {
+			gulp.parallel( js );
+		}
+	} );
+}
 
-gulp.task( "default", [ "watch" ] );
+gulp.task( "watch", gulp.parallel( [ watchFiles, browser_sync ] ) );
